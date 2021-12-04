237c0497fb5824a48664bc17e155ecf4270985e5
flashruler <44584657+flashruler@users.noreply.github.com>
2021-12-04 02:38:55 -0800

    added intermission and matchup

--- front-end/pages/overlay.js
+++ front-end/pages/overlay.js
@@ -1,11 +1,14 @@
 import TopOverlay from '../components/topOverlay.js'
+import Intermission from '../components/intermission.js'
 export default function overlay(props) {
     return (
         <div className=" w-screen h-screen">
+            <Intermission/>
             <div className="flex flex-row justify-center">
-                <TopOverlay/>
+                {/* <TopOverlay/> */}
+                
             </div>
-
+            
 
         </div>
     )
