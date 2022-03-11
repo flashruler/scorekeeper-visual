57dae25c56013b258accc4086281e8d65cff0df6
CameronDeMille <camerondemille2153@gmail.com>
2022-03-10 21:57:14 -0600

    Converted project to be create-react-app with no backend

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
+export default function App() {
+
+  const socketUrl = 'ws://localhost/api/v2/stream/?code=tes'
+  const [messageHistory, setMessageHistory] = useState([]);
+
+  const {
+    lastMessage,
+    readyState,
+  } = useWebSocket(socketUrl);
+
+  useEffect(() => {
+    if (lastMessage !== null) {
+      setMessageHistory(prev => prev.concat(lastMessage));
+    }
+  }, [lastMessage, setMessageHistory]);
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: 'Connecting',
+    [ReadyState.OPEN]: 'Open',
+    [ReadyState.CLOSING]: 'Closing',
+    [ReadyState.CLOSED]: 'Closed',
+    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+  }[readyState];
+
+  return (
+    <div>
+      <span>The WebSocket is currently {connectionStatus}</span>
+      <br></br>
+      <br></br>
+      {lastMessage ? <span>{lastMessage.data}</span> : null}
+      <ul>
+        {messageHistory
+          .map((message, idx) => <span key={idx}>{message ? message.data : null}</span>)}
+      </ul>
+    </div>
+  );
+};
