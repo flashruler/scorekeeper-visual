46010261cbb9452370feba80a3d04f2813ce60fa
flashruler <44584657+flashruler@users.noreply.github.com>
2022-03-19 22:52:46 -0700

    intermission css

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import useWebSocket, { ReadyState } from 'react-use-websocket';
+import React, { useState, useEffect } from "react";
+import useWebSocket, { ReadyState } from "react-use-websocket";
+import PreMatchInformation from "../src/Scenes/preMatchInformation";
 export default function App() {
-
-  const socketUrl = 'ws://localhost/api/v2/stream/?code=tes'
+  const socketUrl = "ws://localhost/api/v2/stream/?code=tes";
   const [messageHistory, setMessageHistory] = useState([]);
 
-  const {
-    lastMessage,
-    readyState,
-  } = useWebSocket(socketUrl);
+  const { lastMessage, readyState } = useWebSocket(socketUrl);
 
   useEffect(() => {
     if (lastMessage !== null) {
-      setMessageHistory(prev => prev.concat(lastMessage));
+      setMessageHistory((prev) => prev.concat(lastMessage));
     }
   }, [lastMessage, setMessageHistory]);
 
   const connectionStatus = {
-    [ReadyState.CONNECTING]: 'Connecting',
-    [ReadyState.OPEN]: 'Open',
-    [ReadyState.CLOSING]: 'Closing',
-    [ReadyState.CLOSED]: 'Closed',
-    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+    [ReadyState.CONNECTING]: "Connecting",
+    [ReadyState.OPEN]: "Open",
+    [ReadyState.CLOSING]: "Closing",
+    [ReadyState.CLOSED]: "Closed",
+    [ReadyState.UNINSTANTIATED]: "Uninstantiated",
   }[readyState];
 
   return (
     <div>
+      <div>
+        <PreMatchInformation />
+      </div>
       <span>The WebSocket is currently {connectionStatus}</span>
       <br></br>
       <br></br>
       {lastMessage ? <span>{lastMessage.data}</span> : null}
       <ul>
-        {messageHistory
-          .map((message, idx) => <span key={idx}>{message ? message.data : null}</span>)}
+        {messageHistory.map((message, idx) => (
+          <span key={idx}>{message ? message.data : null}</span>
+        ))}
       </ul>
     </div>
   );
-};
+}
---
+++ src/Scenes/preMatchInformation.js
@@ -0,0 +1,63 @@
+export default function PreMatchInformation(props) {
+  return (
+    <div className="w-[1920px] h-[1080px] bg-green-500">
+      <div className=" flex flex-row z-10 absolute w-[1177px] h-[91px] ml-[371px] mt-[53px] bg-[#323232] rounded-[8px]  justify-center items-center">
+        <h1 className="text-center text-white leading-[58.59px] text-[50px] italic">
+          San Diego Regional Championship
+        </h1>
+      </div>
+      {/* Matches */}
+      <h1 className="absolute mt-[255px] ml-[41px] text-2xl text-black italic uppercase font-light">
+        Qualification Match 1
+      </h1>
+      {/* Red */}
+      <div className=" flex flex-row absolute w-[660px] h-[57px] mt-[302px] ml-[41px] bg-[#CC4D4D] rounded-md items-center">
+        <h1 className="text-right text-3xl mx-3 font-light italic text-white">
+          4216
+        </h1>
+        <h1 className="text-3xl font-light italic mx-24 text-white uppercase">
+          Rise of Hephaestus
+        </h1>
+        <h1 className="text-3xl font-light italic text-white">1</h1>
+      </div>
+      <div className="flex flex-row  items-center absolute w-[660px] h-[57px] mt-[369px] ml-[41px] bg-[#CC4D4D] rounded-md">
+        <h1 className="text-right text-3xl mx-3 font-light italic text-white">
+          4216
+        </h1>
+        <h1 className="text-3xl font-light italic mx-24 text-white uppercase">
+          Rise of Hephaestus
+        </h1>
+        <h1 className="text-3xl font-light italic text-white">1</h1>
+      </div>
+      {/* Blue */}
+      <div className="flex flex-row  items-center absolute w-[660px] h-[57px] mt-[471px] ml-[41px] bg-[#4885bd] rounded-md">
+        <h1 className="text-right text-3xl mx-3 font-light italic text-white">
+          4216
+        </h1>
+        <h1 className="text-3xl font-light italic mx-24 text-white uppercase">
+          Rise of Hephaestus
+        </h1>
+        <h1 className="text-3xl font-light italic text-white">1</h1>
+      </div>
+      <div className="flex flex-row  items-center absolute w-[660px] h-[57px] mt-[538px] ml-[41px] bg-[#4885bd] rounded-md">
+        <h1 className="text-right text-3xl mx-3 font-light italic text-white">
+          4216
+        </h1>
+        <h1 className="text-3xl font-light italic mx-24 text-white uppercase">
+          Rise of Hephaestus
+        </h1>
+        <h1 className="text-3xl font-light italic text-white">1</h1>
+      </div>
+      <h1 className="absolute z-10 uppercase font-light text-2xl ml-[815px] mt-[208px] text-black italic">
+        Francis Parker High School
+      </h1>
+      <div className="z-10 absolute w-[1011px] h-[569px] ml-[815px] mt-[255px] bg-black"></div>
+      <h1 className="absolute z-10 uppercase font-light text-3xl ml-[1592px] mt-[836px] text-black italic">
+        March 26, 2022
+      </h1>
+      <h1 className="absolute mt-[623px] ml-[156px] text-[200px] italic font-light">
+        4:20
+      </h1>
+    </div>
+  );
+}
