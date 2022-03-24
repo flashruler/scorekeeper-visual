4f4f7f11c3bc4d6ddca5d56bf9986127cb6b8429
flashruler <44584657+flashruler@users.noreply.github.com>
2022-03-24 13:40:48 -0700

    Update matchPlay.js

--- src/Scenes/matchPlay.js
+++ src/Scenes/matchPlay.js
@@ -92,7 +92,7 @@ export default function MatchPlay() {
         />
       </svg>
       {/* Depot Score */}
-      <div className="w-[80px] h-[56px] absolute ml-[696px] mt-[932px] flex flex-row justify-center items-center">
+      <div className="w-[98px] h-[65.48px] absolute ml-[679px] mt-[932px] flex flex-row justify-center items-center">
         <svg
           width="47"
           height="47"
@@ -110,9 +110,109 @@ export default function MatchPlay() {
             stroke-width="5"
           />
         </svg>
-        <h1 className="text-[#323232] justify-self-center text-4xl font-medium text-center ml-2">
+        <h1 className="text-[#323232] justify-self-center text-5xl font-medium text-center ml-2">
           0
         </h1>
+      </div>
+      {/* Hub Score */}
+      <div className="absolute w-[98px] h-[65.48px] ml-[679px] mt-[997px] flex flex-row justify-center items-center">
+        <svg
+          width="53"
+          height="65"
+          viewBox="0 0 53 65"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M53 51.0168C53 52.7431 52.3244 54.4525 51.0119 56.0473C49.6993 57.6422 47.7755 59.0914 45.3502 60.312C42.9249 61.5327 40.0456 62.501 36.8768 63.1616C33.708 63.8222 30.3117 64.1623 26.8818 64.1623C23.4519 64.1623 20.0556 63.8222 16.8868 63.1616C13.718 62.501 10.8388 61.5327 8.41347 60.312C5.98818 59.0914 4.06433 57.6422 2.75176 56.0473C1.4392 54.4525 0.763637 52.7431 0.763638 51.0168L26.8818 51.0168L53 51.0168Z"
+            fill="#323232"
+          />
+          <rect x="24.5506" width="5.00779" height="47.574" fill="#323232" />
+          <rect x="7" y="32" width="40" height="4" fill="#323232" />
+          <rect x="11" y="16" width="31" height="4" fill="#323232" />
+        </svg>
+        <h1 className="text-[#323232] justify-self-center text-5xl font-medium text-center ml-2">
+          0
+        </h1>
+      </div>
+      {/* Red Marker 1*/}
+      <div className="absolute w-[144px] h-[56px] ml-[518px] mt-[867px] flex flex-row justify-center items-center">
+        <svg
+          width="47"
+          height="47"
+          viewBox="0 0 47 47"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            x="2.5"
+            y="2.5"
+            width="42"
+            height="42"
+            rx="7.5"
+            stroke="#323232"
+            stroke-width="5"
+          />
+        </svg>
+        <h1 className="text-5xl text-[#323232] ml-2">DB</h1>
+      </div>
+      {/* Red Marker 2 */}
+      <div className="absolute w-[144px] h-[56px] ml-[518px] mt-[926px] flex flex-row justify-center items-center">
+        <svg
+          width="47"
+          height="47"
+          viewBox="0 0 47 47"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            x="2.5"
+            y="2.5"
+            width="42"
+            height="42"
+            rx="7.5"
+            stroke="#323232"
+            stroke-width="5"
+          />
+        </svg>
+        <h1 className="text-5xl text-[#323232] ml-2">DB</h1>
+      </div>
+      <div className="absolute w-[144px] h-[56px] ml-[518px] mt-[1000px] flex flex-row justify-center items-center">
+        {/* Unparked 1*/}
+        <svg
+          width="32"
+          height="56"
+          viewBox="0 0 32 56"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle cx="16" cy="16" r="14.5" stroke="#323232" stroke-width="3" />
+          <path
+            d="M29.7171 20.5L16 52.2208L2.28289 20.5L29.7171 20.5Z"
+            stroke="#323232"
+            stroke-width="3"
+          />
+          <circle cx="16" cy="16" r="8.5" stroke="#323232" stroke-width="3" />
+          <circle cx="16" cy="16" r="7" fill="white" />
+        </svg>
+        {/* Unparked 2 */}
+        <svg
+          className="ml-3"
+          width="32"
+          height="56"
+          viewBox="0 0 32 56"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle cx="16" cy="16" r="14.5" stroke="#323232" stroke-width="3" />
+          <path
+            d="M29.7171 20.5L16 52.2208L2.28289 20.5L29.7171 20.5Z"
+            stroke="#323232"
+            stroke-width="3"
+          />
+          <circle cx="16" cy="16" r="8.5" stroke="#323232" stroke-width="3" />
+          <circle cx="16" cy="16" r="7" fill="white" />
+        </svg>
       </div>
     </div>
   );
